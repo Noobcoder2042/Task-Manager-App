6129b7ee0cc7b5e0f7adf01f14aa283e1d9b00d8
Rudra Chandra <Rudrachandra117@gmail.com>
2023-06-09 21:35:10 +0530

    done 404

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 const input = document.querySelector(".taskInput");
 const addBtn = document.querySelector(".addBtn");
 const openUL = document.querySelector("#openSection-ul");
-const modalBox = document.querySelector(".modelBox");
+const progressUL = document.querySelector("#progressSection-ul");
+const reviewUL = document.querySelector("#reviewSection-ul");
+const doneUL = document.querySelector("#doneSection-ul");
+const modalBox = document.querySelector("#modalBox");
+const doneButton = document.querySelector(".done");
+const heroProgressSection = document.querySelector(".progressSection");
+const heroReviewSection = document.querySelector(".reviewSection");
+const heroDoneSection = document.querySelector(".doneSection");
 
-let tasks = [];
+let openTasks = [];
+let progressTasks = [];
+let reviewTasks = [];
+let doneTasks = [];
 let draggedItemId = null;
+
+doneButton.addEventListener("click", function () {
+  doneButton.style.display = "none";
+  addBtn.style.display = "none";
+  input.style.display = "none";
+  heroProgressSection.style.display = "block";
+  heroDoneSection.style.display = "block";
+  heroReviewSection.style.display = "block";
+});
 
 function drag(ev) {
   ev.stopPropagation();
   ev.dataTransfer.setData("text", ev.target.id);
+
+  // console.log("drag start");
 }
 
 function allowDrop(ev) {
@@ -23,13 +44,59 @@ function drop(ev) {
   let data = ev.dataTransfer.getData("text");
   const droppedItem = document.getElementById(data);
   const droppedItemId = droppedItem.getAttribute("data-key");
+  const sourceArray = getSourceArray(droppedItem);
+
+  if (sourceArray === openTasks) {
+    openUL.removeChild(droppedItem);
+  } else if (sourceArray === progressTasks) {
+    progressUL.removeChild(droppedItem);
+  } else if (sourceArray === reviewTasks) {
+    reviewUL.removeChild(droppedItem);
+  } else if (sourceArray === doneTasks) {
+    doneUL.removeChild(droppedItem);
+  }
 
   ev.target.appendChild(droppedItem);
 
-  // Remove the dropped item from the tasks array
-  tasks = tasks.filter((task) => task.key !== parseInt(droppedItemId));
+  const destinationArray = getDestinationArray(ev.target);
+  const task = sourceArray.find((task) => task.key === parseInt(droppedItemId));
 
-  console.log(tasks);
+  if (destinationArray === openTasks) {
+    openTasks.push(task);
+  } else if (destinationArray === progressTasks) {
+    progressTasks.push(task);
+    console.log(task.key);
+  } else if (destinationArray === reviewTasks) {
+    reviewTasks.push(task);
+  } else if (destinationArray === doneTasks) {
+    doneTasks.push(task);
+  }
+}
+
+function getSourceArray(item) {
+  if (item.parentElement === openUL) {
+    return openTasks;
+  } else if (item.parentNode === progressUL) {
+    return progressTasks;
+  } else if (item.parentNode === reviewUL) {
+    return reviewTasks;
+  } else if (item.parentNode === doneUL) {
+    return doneTasks;
+  }
+  // console.log(item);
+}
+
+function getDestinationArray(target) {
+  if (target === openUL) {
+    return openTasks;
+  } else if (target === progressUL) {
+    return progressTasks;
+  } else if (target === reviewUL) {
+    return reviewTasks;
+  } else if (target === doneUL) {
+    return doneTasks;
+  }
+  // console.log("getDestinationArray");
 }
 
 addBtn.addEventListener("click", function () {
@@ -37,53 +104,64 @@ addBtn.addEventListener("click", function () {
 
   if (inputVal.trim() !== "") {
     let id = Math.floor(Math.random() * 100);
-    tasks.push({
+    const task = {
       key: id,
       task: inputVal,
       description: "",
-    });
+    };
+    openTasks.push(task);
     input.value = "";
-    renderTask();
-    // console.log(tasks);
+    renderTasks();
   }
 });
 
-// addBtn.addEventListener("click", function () {
-//   const inputVal = input.value;
-
-//   if (inputVal.trim() !== "") {
-//     tasks.push({
-//       key: tasks.length,
-//       task: inputVal,
-//       description: "",
-//     });
-//     input.value = "";
-//     setTimeout(renderTask, 100); // Delay the execution of renderTask
-//     console.log(tasks);
-//   }
-// });
-
-function renderTask() {
+function renderTasks() {
   openUL.innerHTML = "";
-  tasks.map((task) => {
-    if (task.key !== parseInt(draggedItemId)) {
-      const li = document.createElement("li");
-      let id = "id" + Math.floor(Math.random() * 100);
-      li.innerHTML = `<li data-key="${task.key}" id="${id}"
-       draggable='true' class="textBox"
-       ondragstart="drag(event)">${task.task}
-      </li>`;
+  progressUL.innerHTML = "";
+  reviewUL.innerHTML = "";
+  doneUL.innerHTML = "";
 
-      li.addEventListener("click", function () {
-        openModel(task.key);
-      });
-      openUL.append(li);
-    }
+  openTasks.forEach((task) => {
+    const li = createTaskElement(task);
+    openUL.appendChild(li);
+  });
+
+  progressTasks.forEach((task) => {
+    const li = createTaskElement(task);
+    progressUL.appendChild(li);
+  });
+
+  reviewTasks.forEach((task) => {
+    const li = createTaskElement(task);
+    reviewUL.appendChild(li);
+  });
+
+  doneTasks.forEach((task) => {
+    const li = createTaskElement(task);
+    doneUL.appendChild(li);
   });
 }
 
-function openModel(key) {
-  const taskFound = tasks.find((e) => e.key === key);
+function createTaskElement(task) {
+  const li = document.createElement("li");
+  let id = "id" + Math.floor(Math.random() * 100);
+  li.innerHTML = `
+  <li data-key="${task.key}" id="${id}" draggable="true" class="textBox"
+      onclick="openModal(${task.key})" ondragstart="drag(event)">
+    ${task.task}
+  </li>`;
+
+  li.addEventListener("click", function () {
+    openModal(task.key);
+  });
+
+  return li;
+}
+
+function openModal(key) {
+  console.log(key, "openModal");
+  const taskFound = findTaskByKey(key);
+  console.log(taskFound);
   const descriptionInputValue = taskFound.description;
   modalBox.innerHTML = `
     <h2>Task Description</h2>
@@ -98,7 +176,7 @@ function openModel(key) {
 
   const saveBtn = document.querySelector("#save");
   saveBtn.addEventListener("click", function () {
-    saveDes(key);
+    saveDescription(key);
   });
 
   modalBox.style.display = "block";
@@ -108,11 +186,11 @@ function closeModal() {
   modalBox.style.display = "none";
 }
 
-function saveDes(key) {
-  const taskFound = tasks.find((e) => e.key === key);
+function saveDescription(key) {
+  const taskFound = findTaskByKey(key);
   const descriptionInput = document.querySelector(".inputBox");
   taskFound.description = descriptionInput.value;
-  if (taskFound.description.trim() == "") {
+  if (taskFound.description.trim() === "") {
     alert("Can't Save Empty Description");
     taskFound.description = "";
   }
@@ -120,4 +198,24 @@ function saveDes(key) {
   closeModal();
 }
 
-renderTask();
+function findTaskByKey(key) {
+  console.log(key, "findTaskByKey");
+  let task = openTasks.find((e) => e.key === key);
+  if (!task) {
+    task = progressTasks.find((e) => e.key === key);
+  }
+  if (!task) {
+    task = reviewTasks.find((e) => e.key === key);
+  }
+  if (!task) {
+    task = doneTasks.find((e) => e.key === key);
+  }
+  return task;
+}
+const toggleHowButton = document.getElementById("toggleHowButton");
+const howSection = document.querySelector(".how");
+
+toggleHowButton.addEventListener("click", function () {
+  howSection.classList.toggle("hidden");
+});
+doneButton.addEventListener("click", function () {});
